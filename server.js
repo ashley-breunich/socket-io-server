c15fa97e473d63ba4a492c672e9fca724d67e3e0
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2019-01-29 20:58:11 -0800

    added express server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const express = require('express');
+const socketIO = require('socket.io');
 const PORT = process.env.port || 8080;
 
-const io = require('socket.io')(PORT);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
 
 io.on('connection', (socket) => {
     console.log('Connected', socket.id);
@@ -12,3 +18,17 @@ io.on('connection', (socket) => {
     })
 
 });
+
+
+// const PORT = process.env.port || 8080;
+
+// const io = require('socket.io')(PORT);
+
+// io.on('connection', (socket) => {
+//     console.log('Connected', socket.id);
+//     socket.on('troll', (payload) => {
+//         console.log('broadcasting', payload);
+//         io.emit('incoming', payload);
+//     })
+
+// });
